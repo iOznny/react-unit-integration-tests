3c9bb16f0faab8497146f420cc44f6f5f2db228a
iOznny <f000170014@gmail.com>
2021-07-13 20:55:25 -0500

    Cambios

--- src/base/07-deses-arr.js
+++ src/base/07-deses-arr.js
@@ -1,31 +1,22 @@
-
-
 const personajes = ['Goku','Vegeta','Trunks'];
 const [ , , p3 ] = personajes;
-console.log( p3 );
+console.log(p3);
 
-
-const retornaArreglo = () =>{
+const retornaArreglo = () => {
     return ['ABC', 123];
 }
 
 const [ letras, numeros ] = retornaArreglo(); 
 console.log(letras, numeros);
 
-
 // Tarea
 // 1. el primer valor del arr se llamará nombre
 // 2. se llamará setNombre
-const usState = ( valor ) => {
+const usState = (valor) => {
     return [ valor, ()=>{ console.log('Hola Mundo') } ];
 }
 
-const [ nombre, setNombre ] = usState( 'Goku' );
+const [ nombre, setNombre ] = usState('Goku');
 
-console.log( nombre );
+console.log(nombre);
 setNombre();
-
-
-
-
-
--- src/base/09-promesas.js
+++ src/base/09-promesas.js
@@ -17,27 +17,20 @@ import { getHeroeById } from './bases/08-imp-exp'
 // })
 // .catch( err => console.warn( err ) );
 
-const getHeroeByIdAsync = ( id ) => {
-
-    return new Promise( (resolve, reject) => {
-
-        setTimeout( () =>  {
+const getHeroeByIdAsync = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>  {
             // Tarea
-            // importen el 
-            const p1 = getHeroeById( id );
-            if ( p1 ) {
-                resolve( p1 );
+            const p1 = getHeroeById(id);
+            if (p1) {
+                resolve(p1);
             } else {
-                reject( 'No se pudo encontrar el héroe' );
+                reject('No se pudo encontrar el héroe');
             }
-        }, 2000 )
-    
+        }, 2000)
     });
-
-
 }
 
-
 getHeroeByIdAsync(1)
-    .then( console.log )
-    .catch( console.warn );
+    .then(console.log)
+    .catch(console.warn);
