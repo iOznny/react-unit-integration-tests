f2bbdc6b7c88d094e135fccb94814de874a20268
iOznny <f000170014@gmail.com>
2021-07-16 22:24:11 -0500

    Cambios

--- src/base/05-funciones.js
+++ src/base/05-funciones.js
@@ -10,24 +10,14 @@ const saludar2 = (name) => {
 const saludar3 = (name) => `Hola, ${ name }`;
 const saludar4 = () => `Hola Mundo`;
 
-// console.log( saludar('Goku') )
-console.log(saludar2('Vegeta'));
-console.log(saludar3('Goku'));
-console.log(saludar4());
 
-const getUser = () => ({
+export const getUser = () => ({
     uid: 'ABC123',
     username: 'El_Papi1502'
 });
-
-const user = getUser();
-console.log(user);
 
 // Tarea
 const getUsuarioActivo = (name) => ({
     uid: 'ABC567',
     username: name
 });
-
-const usuarioActivo = getUsuarioActivo('Fernando');
-console.log(usuarioActivo);
---
+++ src/tests/base/functions.test.js
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import { getUser, getUsuarioActivo } from '../../base/05-funciones';
+
+describe('Prueba Functions JS', () => {
+    test('getUser return Obj', () => {
+        const userData = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+
+        const user = getUser();
+
+        // toEqual: Permite evaluar las mismas propiedades de los dos elementos (user y userData).
+        expect(user).toEqual(userData);  
+        
+    });
+
+    // getUsuarioActivo
+    test('getUsuarioActivo return Obj', () => {
+        const name = 'Name';
+        const getUser = getUsuarioActivo(name);
+
+        const user = {
+            uid: 'ABC567',
+            username: name
+        };
+
+        expect(getUser).toEqual(user);
+    });
+    
+});
