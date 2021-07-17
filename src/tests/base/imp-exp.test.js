8e73dd2582bf33dcb11ac7a0eaeff94f1cb56c5c
iOznny <f000170014@gmail.com>
2021-07-16 23:20:23 -0500

    Cambios

--- src/tests/base/imp-exp.test.js
+++ src/tests/base/imp-exp.test.js
@@ -2,12 +2,9 @@ import { getHeroeById } from "../../base/08-imp-exp";
 
 describe('Prueba Imp Exp JS', () => {
     test('getHeroeById', () => {
-        
         const id = 1;
         const heroe = getHeroeById(id);
 
         console.log(heroe);
-
-
     });    
 });
