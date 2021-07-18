591f9a4a4d17fac82c4628d521af0e96d8ebfe83
iOznny <f000170014@gmail.com>
2021-07-17 20:19:20 -0500

    Cambios

--- src/tests/base/imp-exp.test.js
+++ src/tests/base/imp-exp.test.js
@@ -1,10 +1,34 @@
-import { getHeroeById } from "../../base/08-imp-exp";
+import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
+import heroes from "../../data/heroes";
 
 describe('Prueba Imp Exp JS', () => {
     test('getHeroeById', () => {
         const id = 1;
         const heroe = getHeroeById(id);
 
-        console.log(heroe);
-    });    
+        const heroeData = heroes.find(h => h.id === id);
+        expect(heroe).toEqual(heroeData);
+    });
+
+    test('getHeroeById undefined', () => {
+        const id = 10;
+        const heroe = getHeroeById(id);        
+        expect(heroe).toBe(undefined);
+    });
+
+    test('getHeroesByOwner by DC', () => {
+        const owner = 'DC';
+        const heroe = getHeroesByOwner(owner);   
+        
+        const heroeData = heroes.filter(h => h.owner === owner);
+        expect(heroe).toEqual(heroeData);
+    });
+    
+    test('getHeroesByOwner by length', () => {
+        const owner = 'Marvel';
+        const heroes = getHeroesByOwner(owner);
+
+        expect(heroes.length).toBe(2);
+       
+    });
 });
