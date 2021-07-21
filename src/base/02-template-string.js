513f62c718e601840f4c7e19ac7f30672c765da7
iOznny <f000170014@gmail.com>
2021-07-20 21:08:44 -0500

    Cambios

--- src/base/02-template-string.js
+++ src/base/02-template-string.js
@@ -1,12 +1,8 @@
 const name = 'Fernando';
 const lastname = 'Herrera';
 
-// const nombreCompleto = nombre + ' ' + apellido;
 const fullname = `${ name } ${ lastname }`;
-//console.log(fullname);
 
 export function getSaludo(name = 'Carlos') {
     return 'Hola ' + name;
 }
-
-//console.log(`Este es un texto: ${ getSaludo(name) } `);
