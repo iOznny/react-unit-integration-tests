29a09a686b64c9f1e5cabaf444e57530937d7198
iOznny <f000170014@gmail.com>
2021-07-21 21:18:43 -0500

    Cambios

--- src/tests/base/async-await.test.js
+++ src/tests/base/async-await.test.js
@@ -3,7 +3,7 @@ import { getImagen } from "../../base/11-async-await";
 describe('Async & Await', () => {
     test('Async & Await Fetch return URL Gif', async () => {
         const url = await getImagen();
-        expect(typeof url).toBe('string');
+        expect(url.includes('https://')).toBe(true);
 
         console.log(url);
     });    
